0571250e831e7c80157c5fda235b879017713a77
Francesco Mergiotti <merjf@Francescos-MacBook-Air.local>
2023-09-04 16:26:22 +0200

    Added Spring Cloud Gateway

--- src/main/java/com/atc/be/gateway/ATCGatewayApplication.java
+++ src/main/java/com/atc/be/gateway/ATCGatewayApplication.java
@@ -1,16 +1,11 @@
 package com.atc.be.gateway;
 
-import com.atc.be.gateway.config.ServiceURLs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ServiceURLs.class)
 public class ATCGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ATCGatewayApplication.class, args);
 	}
-
 }
